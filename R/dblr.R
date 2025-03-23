dd2f1a980ed74df2ce995d29e70e71ba4ebec74d
Christina Sun <christinasun981@gmail.com>
2025-03-23 15:24:50 +1100

    Initial commit

---
+++ R/dblr.R
@@ -0,0 +1,18 @@
+#' function to double things
+#'
+#' This function takes a number and doubles it
+#'
+#' @param x  A number to double
+#' @return The number doubled
+#' @examples
+#' dblr(4)
+#' dblr(0)
+#' @export
+
+dblr <- function(x){
+  return(2*x)
+}
+
+trplr <- function(y){
+  return(3*y)
+}
